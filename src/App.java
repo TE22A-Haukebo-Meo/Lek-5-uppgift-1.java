bed0fcd562bd0e32ffd550f5897386b658dbf1f3
TE22A-Haukebo-Meo <meo.haukebo@elev.ga.ntig.se>
2023-09-25 09:08:12 +0200

    uppgift d

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         int[] fält1 = {8,4,6,3,9};
@@ -11,5 +13,20 @@ public class App {
         System.out.println(summa);
         double medel2 = summa/10;
         System.out.println(medel2);
+
+        int[] summafält = new int [4];
+        summafält [0] = fält1[0]+fält2[0];
+        summafält [1] = fält1[1]+fält2[1];
+        summafält [2] = fält1[2]+fält2[2];
+        summafält [3] = fält1[3]+fält2[3];
+
+        Scanner tb = new Scanner(System.in);
+        int[] fält3 = new int [4];
+        System.out.println("slriv taö");
+        fält3[0] = tb.nextInt();
+        fält3[1] = tb.nextInt();
+        fält3[2] = tb.nextInt();
+        fält3[3] = tb.nextInt();
+        System.out.println(fält3[0]+fält3[1]+fält3[2]+fält3[3]);
     }
 }
