510f13d1a79c8f1657a5bba0441f23d74037bde0
TE22A-Haukebo-Meo <meo.haukebo@elev.ga.ntig.se>
2023-09-25 08:55:07 +0200

    uppgift b

--- src/App.java
+++ src/App.java
@@ -5,5 +5,11 @@ public class App {
         double medel = fält1[0]+fält1[1]+fält1[2]+fält1[3]+fält1[4];
         medel = medel/5;
         System.out.println(medel);
+
+        int[] fält2 = {18,14,16,13,19};
+        int summa = fält1[0]+fält1[1]+fält1[2]+fält1[3]+fält1[4]+fält2[0]+fält2[1]+fält2[2]+fält2[3]+fält2[4];
+        System.out.println(summa);
+        double medel2 = summa/10;
+        System.out.println(medel2);
     }
 }
